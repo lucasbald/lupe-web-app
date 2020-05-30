647c760ffb7f208cee43cfb5a42f505b6a1b3ca8
lucasbald <lucasbaldin1311@gmail.com>
2020-05-30 18:52:34 -0300

    Added icon and updated footer (#10)

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import { MDBCol, MDBContainer, MDBRow, MDBFooter, MDBIcon } from "mdbreact";
+import { MDBCol, MDBContainer, MDBFooter, MDBIcon } from "mdbreact";
  
 const Footer = () => {
     return (
-        <MDBFooter color="mdb-color darken-2" className="font-small pt-4">
-          <MDBContainer fluid className="text-center">
-            <MDBRow>
-              <MDBCol md="12">
-                <h6 className="title">Lup3 Marketing and Tecnology</h6>
-                <p>
-                  Email para contato: lucas.sbaldin@gmail.com
-                </p>
-              </MDBCol>
-            </MDBRow>
-          </MDBContainer>
+        <MDBFooter color="mdb-color darken-2" className="font-small">
           <div className="footer-copyright text-center py-3">
             <MDBContainer fluid>
+            <MDBCol md="12">
+              <h6 className="title">Lup3 Marketing and Tecnology</h6>
+              <p>
+                Email para contato: lucas.sbaldin@gmail.com
+              </p>
+            </MDBCol>
+
               &copy; {new Date().getFullYear()} Copyright 
               <a href="https://github.com/lucasbald/lupe-web-app" className="p-2 fa-lg tw-ic">
                 <MDBIcon fab icon="github" />
