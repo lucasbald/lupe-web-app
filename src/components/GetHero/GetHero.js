55d87e7efb9e684b3d61599cd77e73186dd5abf8
Lucasbald <lucasbaldin1311@gmail.com>
2020-05-31 12:56:20 -0300

    Added get hero first version

--- src/components/GetHero/GetHero.js
+++ src/components/GetHero/GetHero.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from '../../logo.svg';
 import './GetHero.css';
 
 const GetHero = () => {
+  const [heroData, setHeroData] = useState('');
+  const [heroName, setHeroName] = useState('');
+  const [id, setId] = useState(1);
+
+  useEffect(() => {
+    // send HTTP request
+    // save response to variable
+    if (heroName == null || heroName === '') {
+      return;
+    }
+
+    fetch(`https://jsonplaceholder.typicode.com/todos/${heroName}`)
+      .then(results => results.json())
+      .then(data => {
+        setHeroData(data);
+      });
+  }, [heroName, id]); // useEffect will trigger whenever heroName is different.
+
   return (
     <div className="Get-hero">
       <header className="Get-hero-header">
@@ -19,6 +37,27 @@ const GetHero = () => {
           Learn React
         </a>
       </header>
+
+      <h1>Get Your Hero!</h1>
+      <div>
+        <input onChange={e => setId(e.target.value)}/>
+
+        <br></br>
+        <br />
+
+        <button onClick={() => setHeroName(id)}>
+         Ok
+        </button>
+
+        <br></br>
+        <br />
+
+        <pre>{heroData === '' ? '' : JSON.stringify(heroData, null, 2)}</pre>
+        <p>UserId: {heroData.userId}</p>
+        <p>Id: {heroData.id}</p>
+        <p>Title: {heroData.title}</p>
+      </div>
+      
     </div>
   );
 }
