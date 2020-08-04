111dec6faf583bc209ee6142866002c6c356d203
lucasbald <lucasbaldin1311@gmail.com>
2020-08-04 20:03:23 -0300

    Develop (#17)
    * Added icon and updated footer (#10)
    
    * Added icon and updated footer (#10) (#11) (#12)
    
    * Added icon and updated footer (#10) (#11) (#14)
    
    * Feature/router and husky (#13)
    
    * Added icon and updated footer (#10) (#11)
    
    * Removed husky
    
    * Fixed router issue
    
    * Fixed gethero router issue
    
    * Updated get hero link
    
    * Added get hero first version
    
    * Updated color, css and added new template for 404 page
    
    * Test manifest 401 fix
    
    * Disabled header links
    
    * Updated description and added instagram link
    
    * Added prod endpoint
    
    * Added espanish texts
    
    * Added espanish texts

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,47 @@
 import React from 'react';
+import {  MDBRow, MDBCol, MDBCard, MDBCardBody, MDBIcon } from "mdbreact";
+import ImageRounded from "react-rounded-image";
+import lucasImage from '../../assets/lucas-image.jpg';
  
 const About = () => {
     return (
-       <div>
-          <h1>About US</h1>
-          <p>About US page body content</p>
-       </div>
+      <MDBCard className="px-5 pb-1 text-center" color="grey lighten-3">
+      <MDBCardBody>        
+        <br />
+        <MDBRow className="text-md-left">
+          <MDBCol lg="6" md="12" className="mb-5">
+            <MDBCol md="4" lg="6" className="float-left">
+              <ImageRounded
+                image={lucasImage}
+                roundedColor="#321124"
+                imageWidth="200"
+                imageHeight="200"
+                roundedSize="3"
+              />
+
+            </MDBCol>
+            <MDBCol md="8" lg="6" className="float-right">
+              <h4 className="font-weight-bold mb-3">Lucas Baldin</h4>
+              <h6 className="font-weight-bold grey-text mb-3">
+                Ingeniero de Software
+              </h6>
+              <p className="grey-text">
+                Me llamo Lucas Baldin y soy brasileño. Soy informático y tengo una licenciatura en informática. Tengo 25 años.
+              </p>
+              <a href="https://www.linkedin.com/in/lbaldin/" className="p-2 fa-lg fb-ic">
+                <MDBIcon fab icon="linkedin" />
+              </a>
+              <a href="https://github.com/lucasbald" className="p-2 fa-lg tw-ic">
+                <MDBIcon fab icon="github" />
+              </a>
+              <a href="https://www.instagram.com/lucasbald/" className="p-2 fa-lg dribbble-ic">
+                <MDBIcon fab icon="instagram" />
+              </a>
+            </MDBCol>
+          </MDBCol>
+        </MDBRow>
+      </MDBCardBody>
+    </MDBCard>
     );
 }
  
